3c9ecab673ee682b1b6543f2b09a361b29fa2702
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2021-02-12 11:06:47 +0530

    Add files via upload

---
+++ app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { CustomerComponent } from './customer/customer.component';
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  declarations: [
+
+       AppComponent,
+       CustomerComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
